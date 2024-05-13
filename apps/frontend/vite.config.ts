fd83a85a8801f35cf3188bb2bfcc01e015ce30b6
Phạm Hà Anh <89311003+D-RubyIV@users.noreply.github.com>
2024-05-14 03:21:19 +0700

    200

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4173,
+    // hmr: {
+    //   host: "localhost",
+    //   protocol: "ws",
+    // },
+  },
+  preview: {
+    host: true,
+    port: 4173
+  },
+  build: {
+    sourcemap: false,
+  },
 })
