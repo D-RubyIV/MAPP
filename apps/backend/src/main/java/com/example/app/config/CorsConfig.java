205f860a9610835b1676cf56ce0ba28687fb3049
04 DION <phamhaanh2k4.php@gmail.com>
2024-07-28 22:32:09 +0700

    200

--- apps/backend/src/main/java/com/example/app/config/CorsConfig.java
+++ apps/backend/src/main/java/com/example/app/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4173")
+                .allowedOrigins("http://localhost:4173","https://itemjunction.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(false);
     }
